a943fd69eb37d4f856ff28ee552a737f6c9493df
Khairul Anuar <khairul.azizi@mediaprima.com.my>
2019-08-05 17:44:09 +0800

    added few methods

--- src/scrape.js
+++ src/scrape.js
@@ -4,15 +4,17 @@ const puppeteer = require('puppeteer');
 const fetch = require('node-fetch')
 
 class Scrape {
-    constructor(){
+    constructor() {
         var self = this;
         self.dialect = 'sqlite';
         self.storage = './cached_scrape.sqlite';
+        self.sitename = '';
 
 
         self.db = new Sequelize({
             dialect: self.dialect,
-            storage: self.storage
+            storage: self.storage,
+            logging: false
         })
         var sequelize = self.db
 
@@ -43,7 +45,30 @@ class Scrape {
     }
 
     getSite(){
-        
+      
+    }
+
+    isExpired() {
+      var self = this;
+
+    }
+
+    siteExists() {
+      var self = this;
+      return new Promise((resolve, reject) => { 
+        self.sites.findAll({
+          where: {
+            sitename: self.sitename
+          }
+        }).then(function(result){
+          resolve(result.length > 0 ? true:false);
+        })
+      })
+    }
+
+    setSiteName(str) {
+      this.sitename = str
+      return this;
     }
 }
 
